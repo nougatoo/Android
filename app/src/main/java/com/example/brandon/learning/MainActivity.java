41f4b75b6054d267e5bb0ed06e3392b224d70dd4
Brandon Brien <brandon2.brien@gmail.com>
2015-09-17 23:13:19 -0600

    Finished the first couple lessons of the training program

--- app/src/main/java/com/example/brandon/learning/MainActivity.java
+++ app/src/main/java/com/example/brandon/learning/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.brandon.learning;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final static String EXTRA_MESSAGE = "com.mycompany.firstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,9 +20,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -28,16 +33,33 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId())
+        {
+            case R.id.action_search:
+                    //openSearch();
+                    return true;
+            case R.id.action_settings:
+                    openSettings();
+                    return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     //Called when the clicks the send app
     public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageAcivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
 
+        startActivity(intent);
+    }
+
+    //Called when the clicks the send app
+    public void openSettings(){
+        Intent intent = new Intent(this, SettingsActivity.class);
+
+        startActivity(intent);
     }
 }
